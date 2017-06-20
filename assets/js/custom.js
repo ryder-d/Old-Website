2002d8852c8264eaa6434a0e39bfe8684828c3b1
Devon Ryder <devon@devonryder.com>
2017-06-20 11:46:31 -0400

    Removed Typed JS and added code to document ready function.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,10 @@
-// Toggle navbar transparency based on the position of the navbar
-// This will make the navbar opaque once you scroll down the amount of the navbar's height
-$(function () {
-    $(document).scroll(function () {
-        var $navbar = $(".navbar-fixed-top");
-        $navbar.toggleClass('scrolled', $(this).scrollTop() > $navbar.height());
+$(document).ready(function () {
+    // Toggle navbar transparency based on the position of the navbar
+    // This will make the navbar opaque once you scroll down the amount of the navbar's height
+    $(function () {
+        $(document).scroll(function () {
+            var $navbar = $(".navbar-fixed-top");
+            $navbar.toggleClass('scrolled', $(this).scrollTop() > $navbar.height());
+        });
     });
-});
-
-// Typed.js Setup
-$(function(){
-    $(".typed-text").typed({
-        strings: ["Coder.", "Tech enthusiast", "Linux convert in training.",  "Confederation Collge graduate of Computer Programmer."],
-        typeSpeed: 0,
-        showCursor: true,
-        cursorChar: "|",
-        backDelay: 600,
-        loop: true,
-        loopCount: null
-	});
 });
